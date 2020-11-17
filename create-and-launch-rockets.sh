825570bea4e66708df7ff7bf6df80b5d77a1aa61
Olivier Otshudi <olivierots@hotmail.com>
2020-11-17 19:32:17 +0000

    Create create-and-launch-rockets.sh

---
+++ create-and-launch-rockets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mission_name=$1
+
+mkdir $mission_name
+
+rocket-add $mission_name
+
+rocket-start-power $mission_name
+rocket-internal-power $mission_name
+rocket-start-sequence $mission_name
+rocket-start-engine $mission_name
+rocket-lift-off $mission_name
+
+rocket_status=$(rocket-status $mission_name)
+
+echo "The status of launch is $rocket_status"
+
+# while loop before the if conditional statements that checks the status
+# of rocket to wait until the rocket is in a launching state
+
+while [ $rocket_status = "launching" ]
+do
+  sleep 2
+  rocket_status=$(rocket-status $mission_name)
+done
+
+if [ $rocket_status = "failed" ]
+then
+  rocket-debug
+fi
