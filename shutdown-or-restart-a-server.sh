f5fcf52bbe8bc0d5f493a7d56da947a6cc03c17f
Olivier Otshudi <olivierots@hotmail.com>
2020-11-17 19:31:03 +0000

    Create shutdown-or-restart-a-server.sh

---
+++ shutdown-or-restart-a-server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true
+do
+    echo "1. Shutdown"
+    echo "2. Restart"
+    echo "3. Exit Menu"
+	read -p  "Enter your choice: " choice 
+	
+	if [ $choice -eq 1 ]
+	then
+	   shutdown now
+	elif [ $choice -eq 2 ]
+	then 
+	   shutdown -r now 
+	elif [ $choice -eq 3 ]
+	then 
+	   break
+	else
+	   continue
+	fi
+done 
